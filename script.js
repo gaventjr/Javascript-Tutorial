929043767dcee7dfc3a7d5d9f2fc18bd99313d7f
George Avent <gaventjr@gmail.com>
2019-02-26 13:11:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+//let firstName = "George";
+//console.log(firstName);firstName
+
+
+//let age = "32";
+
+//let fullName = (firstName + ' ' + age);
+//alert(fullName);
+
+//let lastName = prompt('What is your last name?');
+//console.log(firstName + ' ' + lastName);
+
+
+/*****Basic operators*/
+//let year = 2019
+//let yearJon = year - 33;
+//let yearMark = year - 28;
+//console.log(yearJon, yearMark);
+//console.log
+
+/*let heightMark = 1.75; //kg
+let heightJon = 1.8; // meters
+
+let massMark =92; // kg
+let massJon = 78; // meters
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJon = massJon / (heightJon * heightJon);
+
+let markHigherBMI = BMIMark > BMIJon;
+
+if (BMIMark > BMIJon) {
+	console.log('Mark\'s BMI is higher than John\'s');
+
+}else{
+	console.log('John\'s BMI is higher than Mark\'s');
+}
+*/
+
+/*var firstName = 'George';
+var age = 50;
+
+if (age < 13) {
+	console.log(firstName + ' is a boy!');
+} else if (age >= 13 && age < 20) {
+	console.log(firstName + ' is a teenager!');
+} else {
+	console.log(firstName + ' is a Grown ASS Man!');
+}
+
+*/
+// Ternary Operator
+//var firstName = 'George';
+//var age = 11;
+
+//age >= 21 ? console.log(firstName + ' drinks beer.')
+//: console.log(firstName + ' drink juice.');
+
+/*var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+*/
+
+//switch Statement
+
+
+
